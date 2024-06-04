b1db7fdd4148c90b2a7cc0757ec7485821bfe84a
Stepan <johan4eg@gmail.com>
2024-06-04 10:26:15 +0300

    Инициализация второго массива, для записи результата. Написан цикл на проверку длины элементов в 1 массиве и записи элементов, прошедших проверку в финальный.

--- finaletask.cs
+++ finaletask.cs
@@ -4,6 +4,17 @@ class Program
     static void Main(string[] args)
     {
         string [] mainarray = {"lol","kek", "cheburek", "kontrosha", "dushit", "abc"};
+        string[] resultarray = new string[mainarray.Length];
+        int resultindex =0;
+        
+        for (int i = 0; i < mainarray.Length; i++)
+        {
+            if (mainarray[i].Length <= 3)
+            {
+                resultarray[resultindex] = resultarray[i];
+                resultindex++;
+            }
+        }
         
     }
 }
