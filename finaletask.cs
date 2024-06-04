d4a01c6101803b65a27ec485abf2ca677bac1f4f
Stepan <johan4eg@gmail.com>
2024-06-04 10:16:05 +0300

    Инициализация массива

---
+++ finaletask.cs
@@ -0,0 +1,9 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string [] mainarray = {"lol","kek", "cheburek", "kontrosha", "dushit", "abc"};
+        
+    }
+}
